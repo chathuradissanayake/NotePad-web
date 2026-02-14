4bc52e4dbc3bd68bb60c638ecaffda99b885ff99
Chathura Dissanayake <chathurakosala6@gmail.com>
2026-02-14 12:55:21 +0530

    fix: add background color and padding to Modal component for improved visibility

--- frontend/src/components/common/Modal.jsx
+++ frontend/src/components/common/Modal.jsx
@@ -12,8 +12,10 @@ const Modal = ({ isVisible, onClose, children, width }) => {
 
   return (
     <div className="fixed inset-0 backdrop-blur-sm bg-opacity-50 flex justify-center items-center z-50" onClick={handleClose}>
-      <div className={`relative  rounded-lg shadow-lg ${width}`}>
-
+      <div className={`relative bg-white p-6 rounded-lg shadow-lg ${width}`}>
+        <button className="absolute top-2 right-2 text-gray-400" onClick={onClose}>
+          <FaTimes />
+        </button>
         {children}
       </div>
     </div>
